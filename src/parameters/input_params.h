240c761d6e92962586d79c8a760c84b496dd46a0
antlju <anton.ljungdahl@gmail.com>
2018-07-02 13:46:21 +0200

    rolling cache bundles!

--- src/parameters/input_params.h
+++ src/parameters/input_params.h
@@ -1,19 +1,22 @@
 #pragma once
 
 
-#define NN 256
+#define NN 32
 #define NX NN
 #define NY NN
 #define NZ NN
 
-/// For bundle/pencil framework
+
+#if __ROLLING_CACHE__ == 1
+
+#define NY_TILE 32
+#define NZ_TILE 32
+#define NX_TILE 1
+
+#else
+
 #define NY_TILE 1
 #define NZ_TILE 1
 #define NX_TILE NN
 
-/// For rolling cache
-/*
-#define NY_TILE 32
-#define NZ_TILE 32
-#define NX_TILE 1
-*/
+#endif
--- src/utility/indexing.h
+++ src/utility/indexing.h
@@ -12,7 +12,17 @@ __host__ __device__ inline size_t fIdx(const Int i, const Int j, const Int k, co
 		+(k+NGHOSTS)+(NX+2*NGHOSTS)*((j+NGHOSTS)+(NY+2*NGHOSTS)*(i+NGHOSTS));
 }
 
+
+__host__ __device__ inline size_t bIdx(const Int i, const Int q, const Int vi=0)
+{
+	//return q*NVARS*(1+2*NG)+vi*(1+2*NG)+(i+NG);
+	return q*(1+2*NG)+(i+NG);
+}
+
+/*
 __host__ __device__ inline size_t bIdx(const Int i, const Int q, const Int vi=0)
 {
 	return q*NVARS*(NX+2*NG)+vi*(NX+2*NG)+(i+NG);
 }
+
+*/
